2f1b2127554b18618cf9041820eb43de3f824e95
Colin Lintereur <colin.lintereur@gmail.com>
2026-03-14 18:30:06 -0400

    Exercise 2.2 (and 2.3)

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -12,11 +12,20 @@ const Content = ({ parts }) => {
   );
 };
 
+const Total = ({ parts }) => (
+  <p>
+    <b>
+      total of {parts.reduce((acc, cur) => acc + cur.exercises, 0)} exercises
+    </b>
+  </p>
+);
+
 const Course = ({ course }) => {
   return (
     <>
       <Header name={course.name}></Header>
       <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
     </>
   );
 };
